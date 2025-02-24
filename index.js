09d453d5897cd3010973ad3faf5a2c15b02b3bed
Karan Modi <kpmodi@my.waketech.edu>
2025-02-24 12:40:48 -0500

    Added all

---
+++ index.js
@@ -0,0 +1,11 @@
+Submit.onclick = function(){
+    document.getElementById("img").src="zombie2.jpg"
+}
+
+Submit2.onclick = function(){
+    document.getElementById("img").src="zombieapoc1.jpg"
+}
+
+restart1.onclick = function(){
+    
+}
