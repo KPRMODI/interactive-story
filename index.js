758232f337cff39dad586d3dd65181b68ca2eb6d
Karan Modi <kpmodi@my.waketech.edu>
2025-02-25 13:05:04 -0500

    ADded stifd

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 Submit.onclick = function(){
     document.getElementById("img").src="zombie2.jpg"
+    document.getElementById("paragraph1").textContent = "You choose to run away, however the zombies are now chasing you! What would you do?"
+    document.getElementById("Submit").textContent = "Disguise yourself"
+    document.getElementById("Submit2").textContent = "Continue Running away"
 }
 
 Submit2.onclick = function(){
